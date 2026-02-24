0d3d7a7c23f0bed81918d715ffb50521ad67241a
PhilKMcC <pkmccaffrey@loyola.edu>
2026-02-24 14:36:38 -0500

    Initial commit

---
+++ Assets/Scripts/TestMove.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TestMove : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        bool a = Input.GetKeyDown(KeyCode.S);
+    }
+}
